0dc5a34817e294eca0a7763c8a0381f60ff8fed9
avan-bre <avan-bre@student.42.fr>
2022-03-10 18:25:13 +0100

    first part parsing

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: avan-bre <avan-bre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 14:45:58 by avan-bre          #+#    #+#             */
-/*   Updated: 2022/03/10 15:14:52 by avan-bre         ###   ########.fr       */
+/*   Updated: 2022/03/10 17:28:02 by avan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,25 @@
 # define CUB3D_H
 
 # include "libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <string.h>
 
 typedef struct s_map
 {
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+	int		*floor;
+	int		*ceiling;
 	char	**map;
 } t_map;
 
 
-int	parse_init_map(char *file);
+int		parse_init_map(t_map *map, char *file);
+void	free_string(char **string);
 
 #endif
--- main.c
+++ main.c
@@ -6,19 +6,48 @@
 /*   By: avan-bre <avan-bre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 14:45:30 by avan-bre          #+#    #+#             */
-/*   Updated: 2022/03/10 15:13:02 by avan-bre         ###   ########.fr       */
+/*   Updated: 2022/03/10 17:52:53 by avan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	free_string(char **string)
+{
+	if (*string)
+	{
+		free(*string);
+		*string = NULL;
+	}
+}
+
+void	ft_free(t_map *map)
+{
+	int i;
+
+	i = -1;
+	while (++i < 3)
+		map->floor[i] = 0;
+	free(map->floor);
+	while (++i < 3)
+		map->ceiling[i] = 0;
+	free(map->ceiling);
+	free_string(&map->no);
+	free_string(&map->so);
+	free_string(&map->we);
+	free_string(&map->ea);
+}
+
 int	main(int argc, char *argv[])
 {
+	t_map	map;
+	
 	if (argc != 2)
 	{
 		printf("Expected format: ./cub3D < mapfile.cub >\n");
 		return (1);
 	}
-	if (!parse_init_map(argv[1]))
+	if (!parse_init_map(&map, argv[1]))
 		return (1);
+	ft_free(&map);
 }
