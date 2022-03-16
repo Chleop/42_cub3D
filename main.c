aae4d05b26966cc6f4cca401b9c36498207c3fea
avan-bre <avan-bre@student.42.fr>
2022-03-16 14:03:53 +0100

    forgot to branch

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cproesch <cproesch@student.42.fr>          +#+  +:+       +#+        */
+/*   By: avan-bre <avan-bre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 14:45:30 by avan-bre          #+#    #+#             */
-/*   Updated: 2022/03/16 13:26:16 by cproesch         ###   ########.fr       */
+/*   Updated: 2022/03/16 13:58:21 by avan-bre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	init_map(t_map *map)
+void	init_struct(t_map *map)
 {
 	map->no = NULL;
 	map->so = NULL;
@@ -56,7 +56,7 @@ int	main(int argc, char *argv[])
 	
 	if (argc != 2)
 		return (error_message("Expected format: ./cub3D < mapfile.cub >", NULL, 1));
-	init_map(&map);
+	init_struct(&map);
 	if (!parse_init_map(&map, argv[1]))
 		free_and_exit(&map, 1);
 	init_window(&map);
